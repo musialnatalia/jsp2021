e83be558463554c9306f96d1c1051378baaecacb
Natalia Posiłek <64209793+musialnatalia@users.noreply.github.com>
2021-10-26 14:09:19 +0200

    Update z5.py
    # 1) Dzielenie bez reszty '//' daje liczbę zmiennoprzecinkową, która jest wynikiem
    #    dzielenia nie biorąc pod uwagę reszty z dzielenia
    # 2) Operacja 'round' zaokrągla liczbę zmiennoprzecinkową, aby stała się liczbą
    #    całkowitą w taki sposób, że cyfrę po kropce mniejszą od 5 zaokrągla w dół,
    #    a większą bądź równą 5 zaokrągla w górę
    # 3) Operacja 'floor' zaokrąga liczbę zmiennoprzecinkową w dół, czyli zostaje tylko
    #	 to co przed kropką

--- z5.py
+++ z5.py
@@ -1,5 +1,14 @@
 import math
 
-print(5//2)
-print(round(float(5/2)))
-print(math.floor(float(5/2)))
+print(5.5/2)
+print(5.//2)
+print(round(5.5/2))
+print(math.floor(5.5/2))
+
+# 1) Dzielenie bez reszty '//' daje liczbę zmiennoprzecinkową, która jest wynikiem
+#    dzielenia nie biorąc pod uwagę reszty z dzielenia
+# 2) Operacja 'round' zaokrągla liczbę zmiennoprzecinkową, aby stała się liczbą
+#    całkowitą w taki sposób, że cyfrę po kropce mniejszą od 5 zaokrągla w dół,
+#    a większą bądź równą 5 zaokrągla w górę
+# 3) Operacja 'floor' zaokrąga liczbę zmiennoprzecinkową w dół, czyli zostaje tylko
+#	   to co przed kropką
