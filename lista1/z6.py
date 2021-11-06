bb8c9d493bf2aa035904645392c8af589e33be6e
Natalia Posiłek <64209793+musialnatalia@users.noreply.github.com>
2021-11-06 16:26:39 +0100

    Delete lista1 directory

--- lista1/z6.py
+++
@@ -1,2 +0,0 @@
-n=2
-print("Wartość pierwiastka drugiego stopnia z liczby −17 : ", (-17)**(1/n))
--- lista1/z9.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cmath
-
-z = complex(input("Podaj liczbę zespoloną 'z': "))
-
-print("|z| = ", np.abs(z))
-print("arg(z) = ", np.degrees(cmath.phase(z)))
-print('conjugate(z) = ', z.conjugate())
