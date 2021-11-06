25344f62ddf3296d8a8a6763028e72dd6e6a80eb
Natalia Posiłek <64209793+musialnatalia@users.noreply.github.com>
2021-11-06 16:34:14 +0100

    Delete lista1 directory

--- lista1/z8.py
+++
@@ -1,9 +0,0 @@
-
-a = float(input("Wprowadź liczbę 'a': "))
-b = float(input("Wprowadź liczbę 'b', która jest mniejsza od liczby 'a': "))
-
-z = a%b
-
-print("Reszta z dzielenia a/b: ", z)
-z*=3+z
-print("Wynik działania z*(z+3): ", z)
